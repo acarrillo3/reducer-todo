227705f14182f1b688c970ee68ca3a766ae695a8
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-01-21 17:32:22 -0500

    step 1 completo

---
+++ to-do/src/reducers/reducer.js
@@ -0,0 +1,33 @@
+import react from 'react'
+
+// Step 1 creat a reducer function and and initial state object
+export const reducer = (state) => {
+    return {
+        ...state,
+        todo: 'new todo'
+    }
+};
+
+export const initialState = [
+    {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+    },
+    {
+        item: 'Learn about context api',
+        completed: false,
+        id: 3892987590
+    },
+    {
+        item: 'Learn about redux',
+        completed: false,
+        id: 3892987591
+    },
+    {
+        item: 'Learn about async redux',
+        completed: false,
+        id: 3892987592
+    },
+
+]
